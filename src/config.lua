4749cfd8f1572754be130e792fd3118265415e32
Georg00808 <scripton0@gmail.com>
2025-12-20 12:53:21 +0100

    fixed

--- src/config.lua
+++ src/config.lua
@@ -1,9 +1,8 @@
 Config = Config or {}
 
--- Add Discord identifiers here to whitelist access to the panel.
--- Example format: "discord:000000000000000000"
+
 Config.AllowedDiscordIds = {
-  "discord:000000000000000000"
+  "discord:1236320445649784904"
 }
 
 Config.DenyMessage = "You are not authorized to open Veracity. Add your Discord ID in src/config.lua."
